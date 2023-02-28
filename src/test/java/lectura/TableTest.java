263ea32cd275ec7850200486bdd312205136fbd8
al414237 <al414237@uji.es>
2023-02-28 22:27:29 +0100

    practica 1 v.1

---
+++ src/test/java/lectura/TableTest.java
@@ -0,0 +1,49 @@
+package lectura;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+    final String RUTA = System.getProperty("user.dir") + "\\src\\main\\java\\archivos\\";
+
+    @Test
+    @DisplayName("Test del numero de filas")
+    public void testRows() throws IOException {
+        CSV csv = new CSV();
+        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
+        assertEquals(25, tabla.numberOfRows());
+    }
+
+    @Test
+    @DisplayName("Test del numero de columnas")
+    public void testColumns() throws IOException {
+        CSV csv = new CSV();
+        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
+        assertEquals(2, tabla.numberOfAttributes());
+    }
+
+    @Test
+    @DisplayName("Test de las etiquetas")
+    public void testHeaders() throws IOException {
+        CSV csv = new CSV();
+        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
+        assertEquals("Miles", tabla.getHeaders().get(0));
+        assertEquals("Dollars", tabla.getHeaders().get(1));
+    }
+
+    @Test
+    @DisplayName("Test de los datos")
+    public void testData() throws IOException {
+        CSV csv = new CSV();
+        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
+        List<Double> datoNum1 = tabla.getRowAt(1).getData();
+        assertEquals(1345, datoNum1.get(0));
+        assertEquals(2405, datoNum1.get(1));
+    }
+
+}
--- src/test/java/lectura/TableWithLabelsTest.java
+++ src/test/java/lectura/TableWithLabelsTest.java
@@ -6,26 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class TablaTest {
+class TableWithLabelsTest {
     final String RUTA = System.getProperty("user.dir") + "\\src\\main\\java\\archivos\\";
-
-    @Test
-    @DisplayName("Test del numero de filas")
-    public void testRowsWithoutLabel() throws IOException {
-        CSV csv = new CSV();
-        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
-        assertEquals(25, tabla.numberOfRows());
-    }
-
-    @Test
-    @DisplayName("Test del numero de columnas")
-    public void testColumnsWithoutLabel() throws IOException {
-        CSV csv = new CSV();
-        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
-        assertEquals(2, tabla.numberOfAttributes());
-    }
 
     @Test
     @DisplayName("Test del numero de filas con etiqueta")
@@ -44,16 +28,6 @@ class TablaTest {
     }
 
     @Test
-    @DisplayName("Test de las etiquetas")
-    public void testLabelWithoutLabel() throws IOException {
-        CSV csv = new CSV();
-        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
-        assertEquals("Miles", tabla.getHeaders().get(0));
-        assertEquals("Dollars", tabla.getHeaders().get(1));
-    }
-
-
-    @Test
     @DisplayName("Test de las etiquetas con etiqueta")
     public void testLabelWithLabel() throws IOException {
         CSV csv = new CSV();
@@ -63,16 +37,6 @@ class TablaTest {
         assertEquals("petal length", tabla.getHeaders().get(2));
         assertEquals("petal width", tabla.getHeaders().get(3));
         assertEquals("class", tabla.getHeaders().get(4));
-    }
-
-    @Test
-    @DisplayName("Test de los datos")
-    public void testDataWithoutLabel() throws IOException {
-        CSV csv = new CSV();
-        Table tabla = csv.readTable(RUTA + "miles_dollars.csv");
-        List<Double> datoNum1 = tabla.getRowAt(1).getData();
-        assertEquals(1345, datoNum1.get(0));
-        assertEquals(2405, datoNum1.get(1));
     }
 
     @Test
